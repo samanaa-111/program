cfc136fa5423abb47b779deb5d559d76bd991b33
samana luitel <samana.luitel@gmail.com>
2026-01-07 21:15:35 +0545

    updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//if,else condition
+//if else condition
 //greatest among three number
     let a=10;
     let b=20;
@@ -28,3 +28,33 @@ else{
 }
 
 //nested if else
+    //Grade calculator
+let score=85;
+if (score>=90){
+    console.log("Your grade is:A");
+}
+else if(score>=80){
+    console.log("Your grade is:B");
+}
+else if (score>=70){
+    console.log("Your grade is:C");
+}
+else{
+    console.log("You are failed");
+}
+
+//Temperature advisor
+let temp="25";
+if (temp<10){
+    console.log("wear a heavy jacket");
+}
+else if(temp>=10 && temp<=20){
+    console.log("A light sweater will do");
+}
+else if(temp>=21 && temp<=30){
+    console.log("Enjoy the t-shirt weather");
+}
+else if(temp>30){
+    console.log("It's hot! stay hydrated");
+}
+
