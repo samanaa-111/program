6832fbd8acf57ada4e8e47141e3bb1f34b803e87
samana luitel <samana.luitel@gmail.com>
2026-01-07 19:53:49 +0545

    https://github.com/samanaa-111/program

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+//if,else condition
+//greatest among three number
     let a=10;
     let b=20;
     let c=25;
@@ -14,3 +15,17 @@
         let msg=`${c} is greatest number`;
         console.log(msg);
     }
+
+// odd or even
+let num=9;
+if (num%2==0){
+    let msg=`${num} is even number`;
+    console.log(msg);
+}
+else{
+    let msg=`${num} is odd number`;
+    console.log(msg);
+}
+
+//nested if else
+    
