f361cc38095d453b2f1093394b69815ec44262dc
samana luitel <samana.luitel@gmail.com>
2026-01-07 19:38:26 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    let a=10;
+    let b=20;
+    let c=25;
+    if(a>b && a>c){
+        let msg=`${a} is greatest number`;
+        console.log(msg);
+    }
+    else if(b>a && b>c){
+        let msg=`${b} is greatest number`;
+        console.log(msg);
+    }
+    else{
+        let msg=`${c} is greatest number`;
+        console.log(msg);
+    }
